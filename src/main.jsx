744a2644d9e35df8894397f5a8769304b1862815
Svitlana Tsupryk <stsupryk@gmail.com>
2025-11-25 15:43:05 +0200

    Changed styles for better UI.

--- src/main.jsx
+++ src/main.jsx
@@ -8,12 +8,12 @@ function Root() {
     <>
       <App />
       <div style={{ position: 'absolute', pointerEvents: 'none', top: 0, left: 0, width: '100vw', height: '100vh' }}>
-        <a href="mailto:stsupryk@gmail.com" target="_blank" style={{ position: 'absolute', bottom: 70, left: 40, fontSize: '16px', color: '#0f4857' }}>
+        <a href="mailto:stsupryk@gmail.com" target="_blank" style={{ position: 'absolute', bottom: 70, left: 40, fontSize: '18px', color: '#0f4857', fontWeight: 'bold' }}>
         <MdEmail style={{ display: 'inline-block', marginBottom: -3, marginRight: 10, width: 20, height: 20, fill: '#0f4857' }} />
           Svitlana Tsupryk
         </a>
-        <div style={{ position: 'absolute', bottom: 70, right: 40, fontSize: '16px', color: '#0f4857' }}>Scroll &#x21e1;&#x21e3;</div>
-        <div style={{ position: 'absolute', top: 50, left: '50%', transform: 'translateX(-50%)', fontSize: '16px', color: '#0f4857' }} href="#">Click on ball &#x2198;</div>
+        <div style={{ position: 'absolute', top: 50, right: 40, fontSize: '20px', color: '#0f4857', fontWeight: 'bold' }}>Scroll &#x21e1;&#x21e3;</div>
+        <div style={{ position: 'absolute', top: 50, left: 40, fontSize: '20px', color: '#0f4857', fontWeight: 'bold' }} href="#">Click on ball &#x2198;</div>
       </div>
     </>
   )
